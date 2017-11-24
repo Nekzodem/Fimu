29e6e9f6729902abfe2d37e1c14d393d3e4c9679
Nekzodem <remi260995@live.fr>
2017-11-24 19:45:17 +0100

    commit

---
+++ fimu.js
@@ -0,0 +1,8 @@
+var icone = document.getElementById("reseau");
+
+function changeColor() {
+
+    reseau.style.backgroundColor = "#blue";
+
+}
+<input type="button" style="border: 1px solid black; background-color: #fff;" Onmousehover="changeColor();" value="2"/>
